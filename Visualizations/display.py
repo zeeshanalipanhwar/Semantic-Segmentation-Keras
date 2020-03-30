a986707fdd303e4ef49d566fb35158b6380132e0
Zeeshan Ali <zapt1860@gmail.com>
2020-03-30 09:10:29 +0500

    Update display.py

--- Visualizations/display.py
+++ Visualizations/display.py
@@ -1,5 +1,5 @@
 import matplotlib.pyplot as plt
-%matplotlib inline
+
 plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
