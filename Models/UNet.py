56d61739b1446b86b247ff81088503701f1fc3b6
Zeeshan Ali <zapt1860@gmail.com>
2020-03-30 13:42:09 +0500

    Update UNet.py

--- Models/UNet.py
+++ Models/UNet.py
@@ -2,7 +2,7 @@ from keras.models import Model
 from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import Conv2D, MaxPooling2D, Conv2DTranspose
 from keras.layers.core import Activation, Flatten, Dropout, Dense
-from keras.layers import Input
+from keras.layers import Input, concatenate
 from keras import backend as K
 
 class UNet:
