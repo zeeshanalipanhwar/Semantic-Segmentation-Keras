a6097acbea79aba0424ddac7d10e2be3fa40e2a9
Zeeshan Ali <zapt1860@gmail.com>
2020-03-31 14:08:54 +0500

    Update layers.py

--- My_Layers/layers.py
+++ My_Layers/layers.py
@@ -1,4 +1,3 @@
-from keras.layers import Activation,BatchNormalization,Conv2D
 from keras.engine.topology import Layer
 import keras.backend as K
 import tensorflow as tf
@@ -9,20 +8,17 @@ class MaxPoolingWithIndices(Layer):
         self.pool_size=pool_size
         self.strides=strides
         self.padding=padding
-        return
-    def call(self,x):
+        
+    def call(self, x):
         pool_size=self.pool_size
         strides=self.strides
-        if isinstance(pool_size,int):
-            ps=[1,pool_size,pool_size,1]
-        else:
-            ps=[1,pool_size[0],pool_size[1],1]
-        if isinstance(strides,int):
-            st=[1,strides,strides,1]
-        else:
-            st=[1,strides[0],strides[1],1]
+        if isinstance(pool_size,int): ps=[1,pool_size,pool_size,1]
+        else: ps=[1,pool_size[0],pool_size[1],1]
+        if isinstance(strides,int): st=[1,strides,strides,1]
+        else: st=[1,strides[0],strides[1],1]
         output1,output2=tf.nn.max_pool_with_argmax(x,ps,st,self.padding)
         return [output1,output2]
+
     def compute_output_shape(self, input_shape):
         if isinstance(self.pool_size,int):
             output_shape=(input_shape[0],input_shape[1]//self.pool_size,input_shape[2]//self.pool_size,input_shape[3])
@@ -30,11 +26,11 @@ class MaxPoolingWithIndices(Layer):
             output_shape=(input_shape[0],input_shape[1]//self.pool_size[0],input_shape[2]//self.pool_size[1],input_shape[3])
         return [output_shape,output_shape]
 
-
 class UpSamplingWithIndices(Layer):
     def __init__(self, **kwargs):
         super(UpSamplingWithIndices, self).__init__(**kwargs)
         return
+        
     def call(self,x):
         argmax=K.cast(K.flatten(x[1]),'int32')
         max_value=K.flatten(x[0])
@@ -50,19 +46,6 @@ class UpSamplingWithIndices(Layer):
             indices=tf.stack([indices_0,indices_1,indices_2,indices_3])
             output=tf.scatter_nd(K.transpose(indices),max_value,output_shape)
             return output
+
     def compute_output_shape(self, input_shape):
         return input_shape[0][0],input_shape[0][1]*2,input_shape[0][2]*2,input_shape[0][3]
-
-def CompositeConv(inputs,num_layers,num_features):
-    output=inputs
-    if isinstance(num_features,int):
-        for i in range(num_layers):
-            output=Conv2D(num_features,(7,7),padding='same')(output)
-            output=BatchNormalization(axis=3)(output)
-            output=Activation('relu')(output)
-        return output
-    for i in range(num_layers):
-        output=Conv2D(num_features[i],(7,7),padding='same')(output)
-        output=BatchNormalization(axis=3)(output)
-        output=Activation('relu')(output)
-    return output
