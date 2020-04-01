d2d54e19a72e400ce4ee7503a9f3021c561257f1
Zeeshan Ali <zapt1860@gmail.com>
2020-04-01 18:19:17 +0500

    Update layers.py

--- My_Layers/layers.py
+++ My_Layers/layers.py
@@ -1,40 +1,38 @@
-from keras.engine.topology import Layer
-import keras.backend as K
 import tensorflow as tf
+from keras.layers import Layer
+from keras import backend as K
 
-class MaxPoolingWithIndices(Layer):
-    def __init__(self, pool_size,strides,padding='SAME',**kwargs):
+class MaxPoolingWithIndices(keras.layers.Layer):
+    def __init__(self, pool_size, strides, padding='SAME', **kwargs):
         super(MaxPoolingWithIndices, self).__init__(**kwargs)
         self.pool_size=pool_size
         self.strides=strides
         self.padding=padding
-        
+
     def call(self, x):
         pool_size=self.pool_size
         strides=self.strides
-        if isinstance(pool_size,int): ps=[1,pool_size,pool_size,1]
+        if isinstance(pool_size, int): ps=[1,pool_size,pool_size,1]
         else: ps=[1,pool_size[0],pool_size[1],1]
         if isinstance(strides,int): st=[1,strides,strides,1]
         else: st=[1,strides[0],strides[1],1]
-        output1,output2=tf.nn.max_pool_with_argmax(x,ps,st,self.padding)
+        output1, output2 = tf.nn.max_pool_with_argmax(x,ps,st,self.padding)
         return [output1,output2]
-
     def compute_output_shape(self, input_shape):
         if isinstance(self.pool_size,int):
             output_shape=(input_shape[0],input_shape[1]//self.pool_size,input_shape[2]//self.pool_size,input_shape[3])
         else:
             output_shape=(input_shape[0],input_shape[1]//self.pool_size[0],input_shape[2]//self.pool_size[1],input_shape[3])
-        return [output_shape,output_shape]
+        return [output_shape, output_shape]
 
 class UpSamplingWithIndices(Layer):
     def __init__(self, **kwargs):
         super(UpSamplingWithIndices, self).__init__(**kwargs)
         return
-        
     def call(self,x):
         argmax=K.cast(K.flatten(x[1]),'int32')
         max_value=K.flatten(x[0])
-        with tf.compat.v1.variable_scope(self.name):
+        with tf.variable_scope(self.name):
             input_shape=K.shape(x[0])
             batch_size=input_shape[0]
             image_size=input_shape[1]*input_shape[2]*input_shape[3]
@@ -46,6 +44,5 @@ class UpSamplingWithIndices(Layer):
             indices=tf.stack([indices_0,indices_1,indices_2,indices_3])
             output=tf.scatter_nd(K.transpose(indices),max_value,output_shape)
             return output
-
     def compute_output_shape(self, input_shape):
         return input_shape[0][0],input_shape[0][1]*2,input_shape[0][2]*2,input_shape[0][3]
