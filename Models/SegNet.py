c783042ae67bb4af4fb830e9dac82909e680147d
Zeeshan Ali <zapt1860@gmail.com>
2020-04-01 18:29:04 +0500

    Update SegNet.py

--- Models/SegNet.py
+++ Models/SegNet.py
@@ -1,97 +1,59 @@
-from keras.models import Model
 from keras.layers.normalization import BatchNormalization
-from keras.layers.convolutional import Conv2D, MaxPooling2D, UpSampling2D
-from keras.layers.core import Activation, Flatten, Dropout, Dense
+from keras.layers.convolutional import Conv2D
 from keras.layers import Input
-from keras import backend as K
+from keras.models import Model
 
 class SegNet:
-    def __init__(self):
-        pass
+    def __init__(self, depth = 64):
+        self.depth = depth
 
-    def encoder(self, input_layer, depth=2):
-        encoded_out = Conv2D(depth, (3, 3), activation='relu', padding="same")(input_layer)
-        encoded_out = BatchNormalization(axis=-1)(encoded_out)
-        encoded_out = Conv2D(depth, (3, 3), activation='relu', padding="same")(encoded_out)
-        encoded_out = BatchNormalization(axis=-1)(encoded_out)
-        encoded_out = MaxPooling2D(pool_size=(2, 2))(encoded_out)
+    def CompositeConv2D(self, input_layer, num_convs, filters):
+        output = input_layer
+        for i in range(num_convs):
+            output = Conv2D(filters, kernel_size=(3, 3), padding='same', activation="relu")(output)
+            output = BatchNormalization(axis=3)(output)
+        return output
+        
+    def encoder(self, input_layer):
+        encoded_out = self.CompositeConv2D(input_layer, 2, self.depth)
+        encoded_out, indices1 = MaxPoolingWithIndices(pool_size=2,strides=2)(encoded_out)
 
-        encoded_out = Conv2D(2*depth, (3, 3), activation='relu', padding="same")(encoded_out)
-        encoded_out = BatchNormalization(axis=-1)(encoded_out)
-        encoded_out = Conv2D(2*depth, (3, 3), activation='relu', padding="same")(encoded_out)
-        encoded_out = BatchNormalization(axis=-1)(encoded_out)
-        encoded_out = MaxPooling2D(pool_size=(2, 2))(encoded_out)
+        encoded_out = self.CompositeConv2D(encoded_out, 2, self.depth*2)
+        encoded_out, indices2 = MaxPoolingWithIndices(pool_size=2,strides=2)(encoded_out)
 
-        encoded_out = Conv2D(4*depth, (3, 3), activation='relu', padding="same")(encoded_out)
-        encoded_out = BatchNormalization(axis=-1)(encoded_out)
-        encoded_out = Conv2D(4*depth, (3, 3), activation='relu', padding="same")(encoded_out)
-        encoded_out = BatchNormalization(axis=-1)(encoded_out)
-        encoded_out = Conv2D(4*depth, (3, 3), activation='relu', padding="same")(encoded_out)
-        encoded_out = BatchNormalization(axis=-1)(encoded_out)
-        encoded_out = MaxPooling2D(pool_size=(2, 2))(encoded_out)
+        encoded_out = self.CompositeConv2D(encoded_out, 3, self.depth*4)
+        encoded_out, indices3 = MaxPoolingWithIndices(pool_size=2,strides=2)(encoded_out)
 
-        encoded_out = Conv2D(8*depth, (3, 3), activation='relu', padding="same")(encoded_out)
-        encoded_out = BatchNormalization(axis=-1)(encoded_out)
-        encoded_out = Conv2D(8*depth, (3, 3), activation='relu', padding="same")(encoded_out)
-        encoded_out = BatchNormalization(axis=-1)(encoded_out)
-        encoded_out = Conv2D(8*depth, (3, 3), activation='relu', padding="same")(encoded_out)
-        encoded_out = BatchNormalization(axis=-1)(encoded_out)
-        encoded_out = MaxPooling2D(pool_size=(2, 2))(encoded_out)
+        encoded_out = self.CompositeConv2D(encoded_out, 3, self.depth*4)
+        encoded_out, indices4 = MaxPoolingWithIndices(pool_size=2,strides=2)(encoded_out)
 
-        encoded_out = Conv2D(8*depth, (3, 3), activation='relu', padding="same")(encoded_out)
-        encoded_out = BatchNormalization(axis=-1)(encoded_out)
-        encoded_out = Conv2D(8*depth, (3, 3), activation='relu', padding="same")(encoded_out)
-        encoded_out = BatchNormalization(axis=-1)(encoded_out)
-        encoded_out = Conv2D(8*depth, (3, 3), activation='relu', padding="same")(encoded_out)
-        encoded_out = BatchNormalization(axis=-1)(encoded_out)
-        encoded_out = MaxPooling2D(pool_size=(2, 2))(encoded_out)
+        encoded_out = self.CompositeConv2D(encoded_out, 3, self.depth*4)
+        encoded_out, indices5 = MaxPoolingWithIndices(pool_size=2,strides=2)(encoded_out)
 
-        return encoded_out
+        return [encoded_out, indices1, indices2, indices3, indices4, indices5]
 
-    def decoder(self, encoded_out, depth=2):
-        decoded_out = UpSampling2D(size=(2, 2))(encoded_out)
-        decoded_out = Conv2D(8*depth, (3, 3), activation='relu', padding="same")(decoded_out)
-        decoded_out = BatchNormalization(axis=-1)(decoded_out)
-        decoded_out = Conv2D(8*depth, (3, 3), activation='relu', padding="same")(decoded_out)
-        decoded_out = BatchNormalization(axis=-1)(decoded_out)
-        decoded_out = Conv2D(8*depth, (3, 3), activation='relu', padding="same")(decoded_out)
-        decoded_out = BatchNormalization(axis=-1)(decoded_out)
+    def decoder(self, encoded_out, indices1, indices2, indices3, indices4, indices5):
+        decoded_out = UpSamplingWithIndices()([encoded_out, indices5])
+        decoded_out = self.CompositeConv2D(decoded_out, 3, self.depth*4)
 
-        decoded_out = UpSampling2D(size=(2, 2))(decoded_out)
-        decoded_out = Conv2D(8*depth, (3, 3), activation='relu', padding="same")(decoded_out)
-        decoded_out = BatchNormalization(axis=-1)(decoded_out)
-        decoded_out = Conv2D(8*depth, (3, 3), activation='relu', padding="same")(decoded_out)
-        decoded_out = BatchNormalization(axis=-1)(decoded_out)
-        decoded_out = Conv2D(8*depth, (3, 3), activation='relu', padding="same")(decoded_out)
-        decoded_out = BatchNormalization(axis=-1)(decoded_out)
+        decoded_out = UpSamplingWithIndices()([decoded_out, indices5])
+        decoded_out = self.CompositeConv2D(decoded_out, 3, self.depth*4)
 
-        decoded_out = UpSampling2D(size=(2, 2))(decoded_out)
-        decoded_out = Conv2D(4*depth, (3, 3), activation='relu', padding="same")(decoded_out)
-        decoded_out = BatchNormalization(axis=-1)(decoded_out)
-        decoded_out = Conv2D(4*depth, (3, 3), activation='relu', padding="same")(decoded_out)
-        decoded_out = BatchNormalization(axis=-1)(decoded_out)
-        decoded_out = Conv2D(4*depth, (3, 3), activation='relu', padding="same")(decoded_out)
-        decoded_out = BatchNormalization(axis=-1)(decoded_out)
+        decoded_out = UpSamplingWithIndices()([decoded_out, indices5])
+        decoded_out = self.CompositeConv2D(decoded_out, 3, self.depth*4)
 
-        decoded_out = UpSampling2D(size=(2, 2))(decoded_out)
-        decoded_out = Conv2D(2*depth, (3, 3), activation='relu', padding="same")(decoded_out)
-        decoded_out = BatchNormalization(axis=-1)(decoded_out)
-        decoded_out = Conv2D(2*depth, (3, 3), activation='relu', padding="same")(decoded_out)
-        decoded_out = BatchNormalization(axis=-1)(decoded_out)
+        decoded_out = UpSamplingWithIndices()([decoded_out, indices5])
+        decoded_out = self.CompositeConv2D(decoded_out, 2, self.depth*2)
 
-        decoded_out = UpSampling2D(size=(2, 2))(decoded_out)
-        decoded_out = Conv2D(depth, (3, 3), activation='relu', padding="same")(decoded_out)
-        decoded_out = BatchNormalization(axis=-1)(decoded_out)
-        decoded_out = Conv2D(depth, (3, 3), activation='relu', padding="same")(decoded_out)
-        decoded_out = BatchNormalization(axis=-1)(decoded_out)
-
+        decoded_out = UpSamplingWithIndices()([decoded_out, indices5])
+        decoded_out = self.CompositeConv2D(decoded_out, 2, self.depth)
+        
         return decoded_out
 
     def SegNet(self, input_shape):
-        depth = 16
         input_layer = Input(shape=input_shape)
-        encoded_out = self.encoder(input_layer, depth)
-        decoded_out = self.decoder(encoded_out, depth)
-        decoded_out = Dense(1, activation="sigmoid")(decoded_out)
+        encoded_out, indices1, indices2, indices3, indices4, indices5 = self.encoder(input_layer)
+        decoded_out = self.decoder(encoded_out, indices1, indices2, indices3, indices4, indices5)
+        decoded_out = Conv2D(1, kernel_size=(1, 1), activation="sigmoid")(decoded_out)
         model = Model(input_layer, decoded_out)
         return model
