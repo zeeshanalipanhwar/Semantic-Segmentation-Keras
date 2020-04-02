78ff915141e4e9e5cd55f2290d6c46efac36f9bb
Zeeshan Ali <zapt1860@gmail.com>
2020-04-02 22:52:56 +0500

    Update SegNet_Configs.py

--- Configs/SegNet_Configs.py
+++ Configs/SegNet_Configs.py
@@ -1,4 +1,7 @@
-#def BATCHSIZE 1
-#def EPOCHS 100
-#def LOSS "binary_crossentropy"
-#def OPTIMISER "adam"
+#define RESHAPE (1024, 1024) # resize the images
+
+#define DEPTH 64 # the depth or the number of filters of the first conv layer of the decoder of the SegNet
+
+#define BATCHSIZE 1
+#define EPOCHS 100
+#define OPTIMIZER "adam"
