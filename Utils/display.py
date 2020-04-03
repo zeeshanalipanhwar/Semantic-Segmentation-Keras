fd3b748934c5bb5324fbd2d68aa4251c32722a47
Zeeshan Ali <zapt1860@gmail.com>
2020-04-03 11:22:07 +0500

    Update display.py

--- Utils/display.py
+++ Utils/display.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
 plt.rcParams['image.interpolation'] = 'nearest'
@@ -10,6 +11,7 @@ def display(image, title=None): # Show image
     plt.axis('off')
     if title: plt.title(title)
     plt.show()
+    return
 
 def display_masked(image, mask, image_name="image", mask_name="mask", cells_color=[1, 1, 0], figsize = (20, 20)):
     '''
@@ -42,3 +44,62 @@ def display_masked(image, mask, image_name="image", mask_name="mask", cells_colo
     plt.title("{} with {} overlapped".format(image_name, mask_name))
     plt.axis('off')
     plt.show()
+    return
+
+def plot_train_valid_loss(H, EPOCHS, save=False, save_to=None, model_name=None):
+    # plot the training and validation losses
+    N = np.arange(0, EPOCHS)
+    plt.figure()
+    plt.plot(N, H.history["loss"], label="Training")
+    plt.plot(N, H.history["val_loss"], label="Validation")
+    plt.title("Loss")
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.legend()
+    if save: plt.savefig("{}{}_basic_loss_plot.png".format(save_to, model_name))
+    plt.show()
+    return
+
+def plot_train_valid_accuracy(H, EPOCHS, save=False, save_to=None, model_name=None):
+    # plot the training and validation accuracies
+    N = np.arange(0, EPOCHS)
+    plt.style.use("ggplot")
+    plt.figure()
+    plt.plot(N, H.history["acc"], label="Training")
+    plt.plot(N, H.history["val_acc"], label="Validation")
+    plt.title("Accuracy")
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy")
+    plt.legend()
+    if save: plt.savefig("{}{}_basic_accuracy_plot.png".format(save_to, model_name))
+    plt.show()
+    return
+
+def plot_train_valid_f1_score(H, EPOCHS, save=False, save_to=None, model_name=None):
+    # plot the training and validation f1 scores
+    N = np.arange(0, EPOCHS)
+    plt.style.use("ggplot")
+    plt.figure()
+    plt.plot(N, H.history["f1_score"], label="Training")
+    plt.plot(N, H.history["val_f1_score"], label="Validation")
+    plt.title("F1 Score")
+    plt.xlabel("Epoch")
+    plt.ylabel("F1 Score")
+    plt.legend()
+    if save: plt.savefig("{}{}_basic_f1_score_plot.png".format(save_to, model_name))
+    plt.show()
+    return
+    
+def plot_train_valid_performance(H, EPOCHS, save_figures=False, save_to=None, model_name=None):
+    if save_figures:
+        if not save_to: raise ValueError("'save_to' must not be 'None' when 'save_figures' is 'True'!")
+        elif not model_name: raise ValueError("'model_name' must not be 'None' when 'save_figures' is 'True'!")
+        else:
+            plot_train_valid_loss(H, EPOCHS, save_figures, save_to, model_name)
+            plot_train_valid_accuracy(H, EPOCHS, save_figures, save_to, model_name)
+            plot_train_valid_f1_score(H, EPOCHS, save_figures, save_to, model_name)
+    else: # save_figures is False
+        plot_train_valid_loss(H, EPOCHS, save_figures, save_to, model_name)
+        plot_train_valid_accuracy(H, EPOCHS, save_figures, save_to, model_name)
+        plot_train_valid_f1_score(H, EPOCHS, save_figures, save_to, model_name)
+    return
