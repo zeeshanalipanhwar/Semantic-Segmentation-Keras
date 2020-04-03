f415b9cd7290ea4abbf836fa710df00b4df0d7bd
Zeeshan Ali <zapt1860@gmail.com>
2020-04-03 23:07:11 +0500

    Update train.py

--- train.py
+++ train.py
@@ -17,3 +17,20 @@ def train_model(model, X_train, Y_train, validation_data, save=False, save_to=No
         model.save("{}{}.model".format(save_to, save_as))
 
     return H # training and validation history
+
+def train_model_on_augmented_data(model, train_data, validation_data, save=False, save_to=None, save_as=None):
+    if save:
+        if not save_to or not save_as:
+            raise ValueError("'save_to' and 'save_as' must not be 'None' when 'save' is 'True'!")
+    # compile the model
+    model.compile(loss="binary_crossentropy", optimizer=SegNet_Configs.OPTIMIZER,
+                  metrics=["accuracy", custom_matrics.f1_score])
+
+    # train the network using augmented data
+    H = model.fit_generator(train_data, validation_data=validation_data,
+                  batch_size=SegNet_Configs.BATCHSIZE, epochs=SegNet_Configs.EPOCHS)
+
+    if save: # save the model for future use or deployment
+        model.save("{}{}.model".format(save_to, save_as))
+
+    return H # training and validation history
