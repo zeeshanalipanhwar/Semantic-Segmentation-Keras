72ccaecede487feae18397aa4eff1c681a57727a
Zeeshan Ali <zapt1860@gmail.com>
2020-04-03 11:49:10 +0500

    Create train.py

---
+++ train.py
@@ -0,0 +1,17 @@
+from Semantic_Segmentation_Keras.Configs import SegNet_Configs
+from Semantic_Segmentation_Keras.Utils import custom_matrics
+
+def train_model(model, save=False, save_as=None):
+    # compile the model
+    model.compile(loss="binary_crossentropy", optimizer=SegNet_Configs.OPTIMIZER,
+                  metrics=["accuracy", custom_matrics.f1_score])
+    
+    # train the network without using augmented data
+    H = model.fit(X_train, Y_train, validation_data=(X_valid, Y_valid),
+                  batch_size=SegNet_Configs.BATCHSIZE,
+                  epochs=SegNet_Configs.EPOCHS)
+    
+    if save: # save the model for future use or deployment
+        if not save_as: raise ValueError("'save_as' must not be 'None' when 'save' is 'True'!")
+        else: model.save("drive/My Drive/segnet_basic.model")
+    return H # training and validation history
