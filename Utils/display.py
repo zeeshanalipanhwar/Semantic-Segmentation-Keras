12b57bf4fd178acdf2ede9a407a0e3d2b765b52e
Zeeshan Ali <zapt1860@gmail.com>
2020-04-03 10:44:21 +0500

    Update display.py

--- Utils/display.py
+++ Utils/display.py
@@ -5,21 +5,30 @@ plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
 def display(image, title=None): # Show image
-    plt.figure(figsize = (5,5))
+    plt.figure(figsize = (10, 10))
     plt.imshow(image)
     plt.axis('off')
     if title: plt.title(title)
     plt.show()
 
-def display_masked(image, mask, image_name, mask_name): # Show image with its segmentation mask
-    masked_image = image.copy()
+def display_masked(image, mask, image_name="image", mask_name="mask", cells_color=[1, 1, 0], figsize = (20, 20)):
+    '''
+    Show image with its segmentation mask
+    '''
+    # resize mask as a three channel image for ploting
     mask_resized = image.copy()
-
     mask_resized[:,:,0] = mask[:,:,0]
     mask_resized[:,:,1] = mask[:,:,0]
     mask_resized[:,:,2] = mask[:,:,0]
 
-    plt.figure(figsize = (20,20))
+    # create a masked image
+    mask_ = mask.copy().round().astype(int)
+    masked_image = image.copy()
+    masked_image[:,:,0][mask_[:,:,0]==1] = cells_color[0]
+    masked_image[:,:,1][mask_[:,:,0]==1] = cells_color[1]
+    masked_image[:,:,2][mask_[:,:,0]==1] = cells_color[2]
+
+    plt.figure(figsize = figsize)
     plt.subplot(1,3,1)
     plt.imshow(image, 'gray')
     plt.title(image_name)
@@ -30,7 +39,6 @@ def display_masked(image, mask, image_name, mask_name): # Show image with its se
     plt.axis('off')
     plt.subplot(1,3,3)
     plt.imshow(masked_image, 'gray')
-    plt.imshow(mask_resized, 'jet', alpha=0.5)
     plt.title("{} with {} overlapped".format(image_name, mask_name))
     plt.axis('off')
     plt.show()
