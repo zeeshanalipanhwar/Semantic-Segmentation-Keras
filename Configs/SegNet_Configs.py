b2e6d133cfb018b4c6e6ba55d44b2dbd58693540
Zeeshan Ali <zapt1860@gmail.com>
2020-04-03 09:10:34 +0500

    Update SegNet_Configs.py

--- Configs/SegNet_Configs.py
+++ Configs/SegNet_Configs.py
@@ -1,7 +1,7 @@
-#define RESHAPE (1024, 1024) # resize the images
+RESHAPE = (1024, 1024) # resize the images
 
-#define DEPTH 64 # the depth or the number of filters of the first conv layer of the decoder of the SegNet
+DEPTH = 64 # the number of filters of the first conv layer of the decoder of the SegNet
 
-#define BATCHSIZE 1
-#define EPOCHS 100
-#define OPTIMIZER "adam"
+BATCHSIZE = 1
+EPOCHS = 100
+OPTIMIZER = "adam"
