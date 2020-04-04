b5a8dedfc71ebacb7dbd9bd1fd3a9b88703e45f9
Zeeshan Ali <zapt1860@gmail.com>
2020-04-04 14:42:12 +0500

    Update UNet_Configs.py

--- Configs/UNet_Configs.py
+++ Configs/UNet_Configs.py
@@ -1,6 +1,6 @@
 RESHAPE = (1024, 1024) # resize the images
 
-DEPTH = 64 # the number of filters of the first conv layer of the decoder of the UNet
+DEPTH = 16 # the number of filters of the first conv layer of the decoder of the UNet
 
 BATCHSIZE = 1
 EPOCHS = 100
