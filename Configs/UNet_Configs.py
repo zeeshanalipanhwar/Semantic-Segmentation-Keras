192f5dbbf71db24d88a935a7b73116938758211b
Zeeshan Ali <zapt1860@gmail.com>
2020-04-04 14:23:42 +0500

    Create UNet_Configs.py

---
+++ Configs/UNet_Configs.py
@@ -0,0 +1,7 @@
+RESHAPE = (1024, 1024) # resize the images
+
+DEPTH = 64 # the number of filters of the first conv layer of the decoder of the UNet
+
+BATCHSIZE = 1
+EPOCHS = 100
+OPTIMIZER = "adam"
