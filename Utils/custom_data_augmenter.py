59d3ba06fda79f6109edde58e70b5c12917866b8
Zeeshan Ali <zapt1860@gmail.com>
2020-04-04 10:55:44 +0500

    Update custom_data_augmenter.py

--- Utils/custom_data_augmenter.py
+++ Utils/custom_data_augmenter.py
@@ -1,11 +1,11 @@
 from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 
-def rotate_segmentation_data(images, masks, percent=1):
+def rotate_segmentation_data(images, masks, percent):
     num_of_images = images.shape[0]
     # include the origional instances to the final list of augmented data
     images_rotated, masks_rotated = list(images), list(masks)
-    for idx in range(0, num_of_images, step=num_of_images*percent):
+    for idx in range(0, num_of_images, int(1/percent)): # 1/percent is the step size
         # rotate the image and its mask by degrees in [90, 270, 360]
         for angle in [90, 270, 360]:
             image_rotated = ImageDataGenerator().apply_transform(images[idx], {'theta': angle})
@@ -16,11 +16,11 @@ def rotate_segmentation_data(images, masks, percent=1):
     masks_rotated = np.array(masks_rotated)
     return images_rotated, masks_rotated
 
-def fliped_segmentation_data_horizontally(images, masks, percent=1):
+def fliped_segmentation_data_horizontally(images, masks, percent):
     num_of_images = images.shape[0]
     # include the origional instances to the final list of augmented data
     images_fliped_horizontally, masks_fliped_horizontally = list(images), list(masks)
-    for idx in range(0, num_of_images, step=num_of_images*percent):
+    for idx in range(0, num_of_images, int(1/percent)): # 1/percent is the step size
         # flip the image and its mask horizontally
         image_fliped_horizontally = ImageDataGenerator().apply_transform(images[idx], {'flip_horizontal': True})
         mask_fliped_horizontally  = ImageDataGenerator().apply_transform(masks[idx], {'flip_horizontal': True})
@@ -30,11 +30,11 @@ def fliped_segmentation_data_horizontally(images, masks, percent=1):
     masks_fliped_horizontally = np.array(masks_fliped_horizontally)
     return images_fliped_horizontally, masks_fliped_horizontally
 
-def fliped_segmentation_data_vertically(images, masks, percent=1):
+def fliped_segmentation_data_vertically(images, masks, percent):
     num_of_images = images.shape[0]
     # include the origional instances to the final list of augmented data
     images_fliped_vertically, masks_fliped_vertically = list(images), list(masks)
-    for idx in range(0, num_of_images, step=num_of_images*percent):
+    for idx in range(0, num_of_images, int(1/percent)): # 1/percent is the step size
         # flip the image and its mask vertically
         image_fliped_vertically = ImageDataGenerator().apply_transform(images[idx], {'flip_vertical': True})
         mask_fliped_vertically  = ImageDataGenerator().apply_transform(masks[idx], {'flip_vertical': True})
@@ -50,7 +50,7 @@ def augment_segmentation_data(images, masks, rotate=False, flip_horizontal=False
     if rotate:
         augmented_images, augmented_masks = rotate_segmentation_data(augmented_images, augmented_masks, rotate_percent)
     if flip_horizontal:
-        augmented_images, augmented_masks = fliped_segmentation_data_horizontally(augmented_images, augmented_masks, flip_horizontal)
+        augmented_images, augmented_masks = fliped_segmentation_data_horizontally(augmented_images, augmented_masks, flip_horizontal_percent)
     if flip_vertical:
         augmented_images, augmented_masks = fliped_segmentation_data_horizontally(augmented_images, augmented_masks, flip_vertical_percent)
     return augmented_images, augmented_masks
