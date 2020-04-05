df649c8a0ca04f039ff684c541047c267323aa98
Zeeshan Ali <zapt1860@gmail.com>
2020-04-05 08:07:36 +0500

    Update load_data2.py

--- Utils/load_data2.py
+++ Utils/load_data2.py
@@ -15,7 +15,7 @@ def load_image(image_path, load_as="rgb"): #Load an image from a file path
     else: color_scheme = cv2.COLOR_BGR2RGB
     return cv2.cvtColor(cv2.imread(image_path), color_scheme)
 
-def split_image_into_subimages(image, sub_image_size=(256, 256), overlap_ratio=.5):
+def split_image_into_subimages(image, sub_image_size, overlap_ratio):
     W_overlap = round(sub_image_size[0] * overlap_ratio)
     H_overlap = round(sub_image_size[1] * overlap_ratio)
     subimages = []
@@ -31,7 +31,7 @@ def split_image_into_subimages(image, sub_image_size=(256, 256), overlap_ratio=.
     subimages = np.array(subimages)
     return subimages
 
-def merge_subimages_into_image(subimages, image_size=(1000, 1000), overlap_ratio=.5):
+def merge_subimages_into_image(subimages, image_size, overlap_ratio):
     W_overlap = round(subimages.shape[0] * overlap_ratio)
     H_overlap = round(subimages.shape[1] * overlap_ratio)
 
@@ -47,15 +47,15 @@ def merge_subimages_into_image(subimages, image_size=(1000, 1000), overlap_ratio
             count += 1
     return image
 
-def load_training_data(files_path, load_as="rgb", sub_images_size=(256, 256)):
+def load_training_data(files_path, load_as="rgb", sub_images_size):
     X_train, Y_train = [], []
 
     for image_path in glob(files_path+'/Training/TissueImages/*'):
         image_name = image_path.split('/')[-1]
         tissue_image = load_image(files_path+'/Training/TissueImages/'+image_name, load_as)
         ground_truth = load_image(files_path+'/Training/GroundTruth/'+image_name.split('.')[0]+'_bin_mask.png', load_as)
-        tissue_subimages = split_image_into_subimages(tissue_image, sub_image_size=(256, 256), overlap_ratio=.5)
-        ground_truth_subimages = split_image_into_subimages(ground_truth, sub_image_size=(256, 256), overlap_ratio=.5)
+        tissue_subimages = split_image_into_subimages(tissue_image, sub_image_size, overlap_ratio)
+        ground_truth_subimages = split_image_into_subimages(ground_truth, sub_image_size, overlap_ratio)
         X_train.append(tissue_subimages)
         Y_train.append(ground_truth_subimages)
 
@@ -73,15 +73,15 @@ def load_training_data(files_path, load_as="rgb", sub_images_size=(256, 256)):
 
     return X_train, Y_train
 
-def load_testing_data(files_path, load_as="rgb", sub_images_size=(256, 256)):
+def load_testing_data(files_path, load_as="rgb", sub_images_size, overlap_ratio):
     X_test, Y_test = [], []
 
     for image_path in glob(files_path+'/Test/TissueImages/*'):
         image_name = image_path.split('/')[-1]
         tissue_image = load_image(files_path+'/Test/TissueImages/'+image_name, load_as)
         ground_truth = load_image(files_path+'/Test/GroundTruth/'+image_name.split('.')[0]+'_bin_mask.png', load_as)
-        tissue_subimages = split_image_into_subimages(tissue_image, sub_image_size=(256, 256), overlap_ratio=.5)
-        ground_truth_subimages = split_image_into_subimages(ground_truth, sub_image_size=(256, 256), overlap_ratio=.5)
+        tissue_subimages = split_image_into_subimages(tissue_image, sub_image_size, overlap_ratio)
+        ground_truth_subimages = split_image_into_subimages(ground_truth, sub_image_size, overlap_ratio)
         X_test.append(tissue_subimages)
         Y_test.append(ground_truth_subimages)
 
@@ -120,17 +120,18 @@ def load_testing_data_as_it_is(files_path, load_as="rgb"):
 
     return X_test, Y_test
 
-def load_data(files_path, load_as="rgb", sub_images_size=(256, 256), validation_size=0.2):
+def load_data(files_path, load_as="rgb", sub_images_size=(256, 256), overlap_ratio=.25, validation_size=0.2):
     '''
     Aurguments:
         files_path: path to the directories that contains the expected Test and Training data directories
         load_as: ("rgb" by default)
         sub_images_size: ((256, 256) by default)
+        overlap_ratio: (.25 by default)
         validation_size: (0.2 by default) it is the ratio of validation data versus train data
     Returns:
     '''
     # loading the train data
-    X_train, Y_train = load_training_data(files_path, load_as="rgb", sub_images_size=(256, 256))
+    X_train, Y_train = load_training_data(files_path, load_as="rgb", sub_images_size, overlap_ratio)
 
     # split the training set to training and validation sets with the ratio 80:20 (by default) or valid_size
     X_train, X_valid, Y_train, Y_valid = train_test_split(X_train, Y_train, test_size=validation_size, random_state=42)
