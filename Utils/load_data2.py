4be5303f5d7e973df4aa1aef8f599b0febfd1320
Zeeshan Ali <zapt1860@gmail.com>
2020-04-06 02:56:54 +0500

    Update load_data2.py

--- Utils/load_data2.py
+++ Utils/load_data2.py
@@ -15,38 +15,6 @@ def load_image(image_path, load_as="rgb"): #Load an image from a file path
     else: color_scheme = cv2.COLOR_BGR2RGB
     return cv2.cvtColor(cv2.imread(image_path), color_scheme)
 
-def split_image_into_subimages(image, sub_image_size, overlap_ratio):
-    W_overlap = round(sub_image_size[0] * overlap_ratio)
-    H_overlap = round(sub_image_size[1] * overlap_ratio)
-    subimages = []
-    for i in range(0, image.shape[0], sub_image_size[0]-W_overlap):
-        x1 = i
-        if x1+sub_image_size[0] <= image.shape[0]: x2 = x1+sub_image_size[0]
-        else: x1, x2 = image.shape[0]-sub_image_size[0], image.shape[0]
-        for j in range(0, image.shape[1], sub_image_size[1]-H_overlap):
-            y1 = j
-            if y1+sub_image_size[1] <= image.shape[1]: y2 = y1+sub_image_size[1]
-            else: y1, y2 = image.shape[1]-sub_image_size[1], image.shape[1]
-            subimages.append(image[x1:x2, y1:y2, :].copy())
-    subimages = np.array(subimages)
-    return subimages
-
-def merge_subimages_into_image(subimages, image_size, overlap_ratio):
-    W_overlap = round(subimages.shape[0] * overlap_ratio)
-    H_overlap = round(subimages.shape[1] * overlap_ratio)
-
-    count = 0
-    image = np.zeros((image_size[0], image_size[1], subimages[0].shape[-1]))
-    for i in range(0, image_size[0], W_overlap):
-        if x1+subimages.shape[0] <= image_size[0]: x2 = x1+subimages.shape[0]
-        else: x1, x2 = image_size[0]-subimages.shape[0], image_size[0]
-        for j in range(0, image_size[1], H_overlap):
-            if y1+subimages.shape[1] <= image_size[1]: y2 = y1+subimages.shape[1]
-            else: y1, y2 = image_size[1]-subimages.shape[1], image_size[1]
-            image[x1:x2, y1:y2, :] = subimages[count].copy()
-            count += 1
-    return image
-
 def load_training_data(files_path, load_as, sub_images_size, overlap_ratio):
     X_train, Y_train = [], []
 
@@ -54,8 +22,8 @@ def load_training_data(files_path, load_as, sub_images_size, overlap_ratio):
         image_name = image_path.split('/')[-1]
         tissue_image = load_image(files_path+'/Training/TissueImages/'+image_name, load_as)
         ground_truth = load_image(files_path+'/Training/GroundTruth/'+image_name.split('.')[0]+'_bin_mask.png', load_as)
-        tissue_subimages = split_image_into_subimages(tissue_image, sub_images_size, overlap_ratio)
-        ground_truth_subimages = split_image_into_subimages(ground_truth, sub_images_size, overlap_ratio)
+        tissue_subimages = custom_images_spliter_and_merger.split_image_into_subimages(tissue_image, sub_images_size, overlap_ratio)
+        ground_truth_subimages = custom_images_spliter_and_merger.split_image_into_subimages(ground_truth, sub_images_size, overlap_ratio)
         X_train.append(tissue_subimages)
         Y_train.append(ground_truth_subimages)
 
@@ -80,8 +48,8 @@ def load_testing_data(files_path, load_as, sub_images_size, overlap_ratio):
         image_name = image_path.split('/')[-1]
         tissue_image = load_image(files_path+'/Test/TissueImages/'+image_name, load_as)
         ground_truth = load_image(files_path+'/Test/GroundTruth/'+image_name.split('.')[0]+'_bin_mask.png', load_as)
-        tissue_subimages = split_image_into_subimages(tissue_image, sub_images_size, overlap_ratio)
-        ground_truth_subimages = split_image_into_subimages(ground_truth, sub_images_size, overlap_ratio)
+        tissue_subimages = custom_images_spliter_and_merger.split_image_into_subimages(tissue_image, sub_images_size, overlap_ratio)
+        ground_truth_subimages = custom_images_spliter_and_merger.split_image_into_subimages(ground_truth, sub_images_size, overlap_ratio)
         X_test.append(tissue_subimages)
         Y_test.append(ground_truth_subimages)
 
