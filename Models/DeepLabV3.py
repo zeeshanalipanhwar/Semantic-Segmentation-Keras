8621764612c42aad160ba7b62f52473da9a0738a
Zeeshan Ali <zapt1860@gmail.com>
2020-04-05 23:40:42 +0500

    Update DeepLabV3.py

--- Models/DeepLabV3.py
+++ Models/DeepLabV3.py
@@ -1,7 +1,7 @@
 from keras.models import Model
 from keras.layers.normalization import BatchNormalization
 from keras.layers.convolutional import Conv2D, MaxPooling2D
-from keras.layers.core import Activation, Flatten, Dropout, Dense
+from keras.layers.core import Activation, Dropout, Dense
 from keras.layers import Input, concatenate, UpSampling2D
 from keras import backend as K
 
@@ -56,7 +56,7 @@ class DeepLabV3:
         output_layer = MaxPooling2D(pool_size=(2, 2))(output_layer)
         output_layer = Dropout(dropout)(output_layer)
         
-        # Block four that retains 16 using Atrous Convolution
+        # Block four that retains 8 using Atrous Convolution
         output_layer = Conv2D(self.depth*8, (3, 3), dilation_rate=(2,2), activation='relu', padding="same")(output_layer)
         output_layer = BatchNormalization()(output_layer)
         output_layer = Conv2D(self.depth*8, (3, 3), dilation_rate=(2,2), activation='relu', padding="same")(output_layer)
@@ -69,11 +69,12 @@ class DeepLabV3:
 
         # Block six of concatination and then a 1x1 conv on the concatenated output
         concatenated = concatenate([conv11_layer, atrous_conv1, atrous_conv2, atrous_conv3, maxpooled_in])        
-        output_layer = Conv2D(1, (1, 1), activation="softmax", padding="same")(concatenated)
-        output_layer = BatchNormalization()(output_layer)
+        output_layer = Conv2D(1, (1, 1), activation="relu", padding="same")(concatenated)
         
         # Block seven of 8 times upsampling of the output using Bilinear interpolation
         output_layer = UpSampling2D(size=(8, 8), data_format=None, interpolation='bilinear')(output_layer)
+        
+        output_layer = Activation("softmax")(output_layer)
         
         # Create the model using the input layer and the final output layer
         model = Model(input_layer, output_layer)
