5c14c8d95a1448ca15935a843cbf26fcd2ad2e53
Zeeshan Ali <zapt1860@gmail.com>
2020-04-05 17:37:40 +0500

    Update UNet_Configs.py

--- Configs/UNet_Configs.py
+++ Configs/UNet_Configs.py
@@ -1,7 +1,6 @@
-RESHAPE = (1024, 1024) # resize the images
+DEPTH = 16 # the number of filters of the first conv layer of the encoder of the UNet
 
-DEPTH = 16 # the number of filters of the first conv layer of the decoder of the UNet
-
-BATCHSIZE = 1
+# Training hyperparameters
+BATCHSIZE = 16
 EPOCHS = 100
 OPTIMIZER = "adam"
